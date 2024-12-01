c2d36098a7b516efd2ced61b931329fdfc242f1a
Maxim <mgrochitskiy@mail.ru>
2024-12-01 18:34:09 +0600

    Доделан unit-test

---
+++ src/test/java/TestTask.java
@@ -0,0 +1,14 @@
+import org.example.StringProcessor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTask {
+
+    @Test
+    public void TestMultiplyString()
+    {
+       StringProcessor test = new StringProcessor();
+       assertEquals("Строчка должна повториться N кол-во раз",test.multiplyString("A",5),"AAAAA");
+    }
+}
