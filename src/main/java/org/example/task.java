1804691c1285590d5eb7e956bd5c580ff9699ba9
Maxim <mgrochitskiy@mail.ru>
2024-12-01 17:58:00 +0600

    Был свомещен java класс в один

--- src/main/java/org/example/task.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class task {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/teststask.java
@@ -0,0 +1,13 @@
+import org.example.StringProcessor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class teststask {
+
+    @Test
+    public void TestmultiplayString()
+    {
+
+    }
+}
