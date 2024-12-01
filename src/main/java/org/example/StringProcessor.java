fcf831eb6afaa335adca84453c9535fe80ce37fe
Maxim <mgrochitskiy@mail.ru>
2024-12-01 20:14:00 +0600

    Добавлен новый метода для задачи №2, а также было создано под метод unit-test. Переработан unit-test для метода задачи №1 - добавлено тест для исключений

--- src/main/java/org/example/StringProcessor.java
+++ src/main/java/org/example/StringProcessor.java
@@ -2,8 +2,9 @@ package org.example;
 
 
 public class StringProcessor {
-
-    public String multiplyString(String s, int n) {
+    public String multiplyString(String s, int n)
+    //Задача №1
+    {
         String result = "";
         if (n < 0) {
             throw new IllegalArgumentException("Exception: N < 0 ");
@@ -12,5 +13,19 @@ public class StringProcessor {
             result = result.concat(s);
         }
         return result;
+    }
+    public int amountString2intoString1(String s1, String s2)
+    //Задача №2
+    {
+        if (s2 == null || s2.isEmpty()) {
+            throw new IllegalArgumentException("Exception: s2 = null");
+        }
+        int count = 0;
+        int index = 0;
+        while ((index = s1.indexOf(s2,index)) != -1) {
+            count++;
+            index += s2.length();
+        }
+        return count;
     }
 }
--- src/test/java/TestTask.java
+++ src/test/java/TestTask.java
@@ -1,15 +1,55 @@
 import org.example.StringProcessor;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestTask {
 
-    @Test
+        @Test
     public void TestMultiplyString()
+            // Для задачи №1
     {
        StringProcessor test = new StringProcessor();
-       assertEquals("Строчка должна повториться N кол-во раз",test.multiplyString("A",5),"AAAAA");
-        assertEquals("Строчка должна быть пустой",test.multiplyString("A",0),"");
+       assertEquals(
+               "Строчка должна повториться N кол-во раз",
+               test.multiplyString("A",5),
+               "AAAAA"
+       );
+        assertEquals(
+                "Строчка должна быть пустой",
+                test.multiplyString("A",0),
+                ""
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+            test.multiplyString("AAAA", -1);
+        }
+        );
+    }
+@Test
+    public void TestAmountString2intoString1()
+    {
+        StringProcessor test = new StringProcessor();
+        assertEquals(
+                "В строчке должно быть заданое кол-во подстрок",
+                test.amountString2intoString1("hellohello","hello"),
+                2
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    // Код, который должен выбросить исключение
+                    test.amountString2intoString1("hello", null);
+                }
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    // Код, который должен выбросить исключение
+                    test.amountString2intoString1("hello", "");
+                }
+        );
     }
 }
