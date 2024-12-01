dc578bc898f4d303d5a93a0e32e682ca48f592d7
Maxim <mgrochitskiy@mail.ru>
2024-12-01 18:35:01 +0600

    Класс был переработан под unit тестирование

--- src/main/java/org/example/StringProcessor.java
+++ src/main/java/org/example/StringProcessor.java
@@ -1,21 +1,12 @@
 package org.example;
 
-import java.util.Scanner;
 
 public class StringProcessor {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        StringProcessor test = new StringProcessor();
-        String s = "a";
-        int n = 5;
-        System.out.println(test.multiplayString(s,n));
 
-        scanner.close();
-    }
-    public String multiplayString(String s, int n) {
+    public String multiplyString(String s, int n) {
         String result = "";
-        if (s == null || n < 0) {
-            throw new NullPointerException("Exception: s = null");
+        if (n < 0) {
+            throw new IllegalArgumentException("Exception: N < 0 ");
         }
         for (int i = 0; i < n; i++) {
             result = result.concat(s);
