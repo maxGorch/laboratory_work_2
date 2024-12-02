768355bfe19dbcb4f81f3bc4634cb512bafae0d8
Maxim <mgrochitskiy@mail.ru>
2024-12-02 21:42:49 +0600

    Был создан новый класс для выполнения задания №7 и создан под класс unit-test

---
+++ src/main/java/org/example/Payment.java
@@ -0,0 +1,128 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Payment
+//TODO: Задача №7
+// 1. Создайте класс Payment (платеж) с полями:
+//  ФИО человека (одна строка),
+//  дата  платежа — число,
+//  месяц и год (целые числа),
+//  сумма платежа (целое число — сумма  в копейках).
+// 2. Напишите необходимые
+// конструкторы, геттеры/сеттеры, методы  equals, hashCode, toString.
+{
+    String  SNM;//  ФИО человека (одна строка),
+    int payment_date;//  дата  платежа — число,
+    int month;//месяц (целые числа)
+    int year;// год (целые числа),
+    int payment_amount;//  сумма платежа (целое число — сумма  в копейках).
+
+    public Payment(String SNM,int payment_date,int month,int year,int payment_amount) {
+        if(SNM == null || SNM.isEmpty())
+        {
+            throw new IllegalArgumentException("Ожидалось передача значения для SNM!");
+        }
+        else this.SNM = SNM;
+        if(payment_date <1 || payment_date >31)
+        {
+            throw new IllegalArgumentException("Дата не должна выходить за пределы 31 или быть меньше 1!");
+        }
+        else this.payment_date = payment_date;
+        if(payment_amount <1)
+        {
+            throw new IllegalArgumentException("Платеж не может быть отрицательным!");
+        }
+        else this.payment_amount=payment_amount;
+        if(month <1 || month >12)
+        {
+            throw new IllegalArgumentException("Месяц не должен выходить за пределы 12 или быть меньше 1!");
+        }
+        else this.month=month;
+        if(year <1)
+        {
+            throw new IllegalArgumentException("Год не может быть отрицательным!");
+        }
+        else this.year=year;
+    }
+    //============Геттеры============
+    public String getSNM(){return SNM;}
+    public int getPayment_date(){return payment_date;}
+    public int getMonth(){return month;}
+    public int getYear(){return year;}
+    public int getPayment_amount(){return payment_amount;}
+    //============Сеттер============
+    public void setSNM(String SNM){
+        if(SNM == null || SNM.isEmpty())
+        {
+            throw new IllegalArgumentException("Ожидалось передача значения для SNM!");
+        }
+        else this.SNM = SNM;
+    }
+    public void setPayment_date(int payment_date){
+        if(payment_date <1 || payment_date >31)
+        {
+            throw new IllegalArgumentException("Дата не должна выходить за пределы 31 или быть меньше 1!");
+        }
+        this.payment_date=payment_date;
+    }
+    public void setYear(int year){
+        if(year <1)
+        {
+            throw new IllegalArgumentException("Год не может быть отрицательным!");
+        }
+        else this.year=year;
+    }
+    public void setMonth(int month){
+        if(month <1 || month >12)
+        {
+            throw new IllegalArgumentException("Месяц не должен выходить за пределы 12 или быть меньше 1!");
+        }
+        else this.month=month;
+    }
+    public void setPayment_amount(int payment_amount){
+
+        if(payment_amount <1)
+        {
+            throw new IllegalArgumentException("Платеж не может быть отрицательным!");
+        }
+        else this.payment_amount=payment_amount;
+    }
+    //============Метод Equals============
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) { // проверка на идентичность
+            return true;
+        }
+        if (!(obj instanceof Payment other)) { // проверка типа объекта
+            return false;
+        }
+        return this.SNM.equals(other.SNM) &&
+                this.payment_date == other.payment_date &&
+                this.payment_amount == other.payment_amount &&
+                this.month == other.month &&
+                this.year == other.year; // сравнение содержимого
+    }
+
+    //============Метод hashCode============
+    //используется для быстрого сравнения.
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(SNM, payment_date, month,year, payment_amount);
+    }
+
+    //============Метод toString============
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "SNM='" + SNM + '\'' +
+                ", payment_date=" + payment_date +
+                ", month=" + month +
+                ", year=" + year +
+                ", payment_amount=" + payment_amount +
+                '}';
+    }
+}
---
+++ src/test/java/TestPayment.java
@@ -0,0 +1,74 @@
+import org.example.Payment;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPayment {
+@Test
+    public void testPayment()
+    {
+        Payment new_payment = new Payment("ГМЮ", 1, 12,2024,1500);
+        assertEquals(
+                "Нужно задать имя и все основные цифры для подачи документа",
+                new_payment.toString(),
+                "Payment{" +
+                        "SNM='ГМЮ"  + '\'' +
+                        ", payment_date=1" +
+                        ", month=12" +
+                        ", year=2024" +
+                        ", payment_amount=1500" +
+                        '}'
+
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("ГМЮ", -1, 12, 2024, 1500); // дата < 1
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("ГМЮ", 1, 13, 2024, 1500); // месяц > 12
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("ГМЮ", 1, 12, -2024, 1500); // год < 1
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("ГМЮ", 1, 12, 2024, -1500); // сумма < 1
+        });
+
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setSNM("");
+        },"IllegalArgumentException при передаче empty строки");
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setSNM(null);
+        },"IllegalArgumentException при передаче null строки");
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setPayment_date(-1);
+        },"IllegalArgumentException при передаче в Дату значения меньше 1 ");
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setPayment_date(32);
+        },"IllegalArgumentException при передаче в Дату значения больше 31 ");
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setMonth(-1);
+        },"IllegalArgumentException при передаче в Месяц значения меньше 1 ");
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setPayment_date(32);
+        },"IllegalArgumentException при передаче в Месяц значения больше 12 ");
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setPayment_amount(-1);
+        },"IllegalArgumentException при передаче в Сумму платежа значения меньше 1 ");
+
+        assertThrows(IllegalArgumentException.class,()->{
+            new_payment.setYear(-1);
+        },"IllegalArgumentException при передаче в Год значения меньше 1 ");
+    }
+
+}
